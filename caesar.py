c9e7007ddb7b9b13c2a7262ab4c860de8a838d1b
Destin Mortys <32856270+DestinMortys@users.noreply.github.com>
2023-09-24 17:41:59 +0000

    Formatage files

--- caesar.py
+++ caesar.py
@@ -4,66 +4,72 @@ intercepted_message = 'fytgpcdtep op dspcmczzvp'
 
 def encrypt(clear_text):
     # Génération aléatoire de la clé dans la plage de 1 à 25 inclus
-    key = random.randint(1, 25)
 
     cipher_text = ""
-    
+
     for char in clear_text:
+        key = random.randint(1, 25)
         if 'a' <= char <= 'z':
-            encrypted_char = chr(((ord(char) - ord('a') + key) % 26) + ord('a'))      
+            encrypted_char = chr(
+                ((ord(char) - ord('a') + key) % 26) + ord('a'))
         elif 'A' <= char <= 'Z':
             char = char.lower()
-            encrypted_char = chr(((ord(char) - ord('a') + key) % 26) + ord('a'))
+            encrypted_char = chr(
+                ((ord(char) - ord('a') + key) % 26) + ord('a'))
         else:
             # Caractères non alphabétiques, on les laisse inchangés
             encrypted_char = char
         cipher_text += encrypted_char
-    
-    return cipher_text
 
+    return cipher_text
 
 
 def decrypt(cipher_text):
     clear_text = ""
-    
+
     for key in range(26):
         decrypted_text = ""
         for char in cipher_text:
             if 'a' <= char <= 'z':
-                decrypted_char = chr(((ord(char) - ord('a') - key) % 26) + ord('a'))
+                decrypted_char = chr(
+                    ((ord(char) - ord('a') - key) % 26) + ord('a'))
             elif 'A' <= char <= 'Z':
                 char = char.lower()
-                decrypted_char = chr(((ord(char) - ord('a') - key) % 26) + ord('a'))
+                decrypted_char = chr(
+                    ((ord(char) - ord('a') - key) % 26) + ord('a'))
             else:
                 decrypted_char = char
             decrypted_text += decrypted_char
-        
+
         # Vérifier si le texte déchiffré est une option valide
         if decrypted_text.islower():
             clear_text = decrypted_text
             break
-    
+
     return clear_text
 
 
 def brute_force(intercepted_message):
     brute_force_dict = {}
-    
+
     for key in range(26):  # Il y a 26 décalages possibles dans l'alphabet
         clear_text = ""
         for char in intercepted_message:
             if char.isalpha():
                 if char.isupper():
-                    decrypted_char = chr(((ord(char) - ord('A') - key) % 26) + ord('A'))
+                    decrypted_char = chr(
+                        ((ord(char) - ord('A') - key) % 26) + ord('A'))
                 else:
-                    decrypted_char = chr(((ord(char) - ord('a') - key) % 26) + ord('a'))
+                    decrypted_char = chr(
+                        ((ord(char) - ord('a') - key) % 26) + ord('a'))
                 clear_text += decrypted_char
             else:
                 clear_text += char
-        
+
         brute_force_dict[key] = clear_text
-    
+
     return brute_force_dict
+
 
 # Attaque de force brute
 result = brute_force(intercepted_message)
--- substitution.py
+++ substitution.py
@@ -3,37 +3,36 @@ from binascii import hexlify
 import textwrap
 
 
-def freq_text( clear_text_hugo:str )->list:
-  freq = {}
-  for letter in clear_text_hugo:
+def freq_text(clear_text_hugo: str) -> list:
+    freq = {}
+    for letter in clear_text_hugo:
         if letter == '\n':
-          continue
+            continue
         if letter in freq:
-                freq[letter] += 1
+            freq[letter] += 1
         else:
-                freq[letter] = 1
-  return sorted(freq.items(), key=lambda item : item[1], reverse=True)
+            freq[letter] = 1
+    return sorted(freq.items(), key=lambda item: item[1], reverse=True)
 
 
-def freq_cipher( cipher_text_1:bytes ) ->list:
-  freq = {}
-  for index in range(len(cipher_text_1)):
-    if index%2 == 0:
-            char = cipher_text_1[index:index+2] 
+def freq_cipher(cipher_text_1: bytes) -> list:
+    freq = {}
+    for index in range(len(cipher_text_1)):
+        if index % 2 == 0:
+            char = cipher_text_1[index:index+2]
             if char in freq:
                 freq[char] += 1
             else:
                 freq[char] = 1
-  
-  return sorted(freq.items(), key=lambda item : item[1], reverse=True)
+
+    return sorted(freq.items(), key=lambda item: item[1], reverse=True)
 
 
-
-def guess_clear_text(cipher_text_1:bytes, decryption_key:dict)  -> str:
+def guess_clear_text(cipher_text_1: bytes, decryption_key: dict) -> str:
     clear_text = ""
     for index in range(len(cipher_text_1)):
         if index % 2 == 0:
-            char = cipher_text_1[index:index+2]  
+            char = cipher_text_1[index:index+2]
             clear_char = decryption_key.get(char, "%s" % char)
             clear_text += clear_char
     return clear_text
@@ -41,19 +40,20 @@ def guess_clear_text(cipher_text_1:bytes, decryption_key:dict)  -> str:
 
 def build_decryption_key(freq_clear_text: list, freq_cipher_text: list, key_size: int) -> dict:
     decryption_key = {}
-    
+
     for i in range(min(key_size, len(freq_cipher_text))):
         freq_cipher_char, _ = freq_cipher_text[i]
         freq_clear_char, _ = freq_clear_text[i]
         decryption_key[freq_cipher_char] = freq_clear_char
     return decryption_key
 
+
 if __name__ == '__main__':
-    
+
     cipher_text_1_path = "cipher_text_1.bin"
     cipher_text_2_path = "cipher_text_2.bin"
     text_hugo_path = "text_hugo.txt"
-    
+
     with open(cipher_text_1_path, 'br') as f:
         cipher_text_1 = f.read()
     with open(cipher_text_2_path, 'br') as f:
@@ -64,28 +64,28 @@ if __name__ == '__main__':
     # Obtenir les fréquences des caractères pour clear_text_hugo et cipher_text_1
     freq_hugo = freq_text(clear_text_hugo)
     freq_cipher_text_1 = freq_cipher(cipher_text_1)
-    
 
     # Construire une clé de déchiffrement partielle basée sur les caractères les plus fréquents
     decryption_key = build_decryption_key(freq_hugo, freq_cipher_text_1, 15)
-    
+
     # Devinez le texte clair en utilisant la clé de déchiffrement partielle
-    clear_text_1 = guess_clear_text(cipher_text_1, decryption_key) 
+    clear_text_1 = guess_clear_text(cipher_text_1, decryption_key)
 
-  
-print("*** question 8 " ) 
+
+print("*** question 8 ")
 freq_hugo = freq_text(clear_text_hugo)
-print( "freq_hugo: %s"%freq_hugo ) 
+print("freq_hugo: %s" % freq_hugo)
 
 
-print( "*** question 9" )
+print("*** question 9")
 freq_cipher_text_1 = freq_cipher(cipher_text_1)
 print(freq_cipher_text_1)
 
 
-print( "*** question 11")
-print("decryption_key: %s"%decryption_key)
-print("clear_text_1: %s"%clear_text_1 )
+print("*** question 11")
+clear_text_1 = guess_clear_text(cipher_text_1, decryption_key)
+print("decryption_key: %s" % decryption_key)
+print("clear_text_1: %s" % clear_text_1)
 
 """
 print( "*** question 12" )
@@ -94,7 +94,6 @@ decryption_key = build_decryption_key(  freq_hugo, freq_cipher_text_2, key_size=
 print("decryption_key: %s"%decryption_key)
 clear_text_2 = guess_clear_text( cipher_text_2, decryption_key ) 
 print("clear_text_2: %s"%clear_text_2 )
- 
 
 print( "*** question 13" )
 indice = "Anton Voyl n'arrivait pas à dormir"
